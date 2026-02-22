f280374a85e573b651dc635331fc0c0b602eb15b
mateusfavarin <mateusfavarinc@gmail.com>
2026-02-21 21:20:26 -0800

    decompile Heap_MergeFreeBlocks

--- decomp/src/heap.c
+++ decomp/src/heap.c
@@ -11,11 +11,70 @@ static inline HeapBlock* Heap_NextBlock(HeapBlock* block)
     return Heap_AdvanceWords(block, block->header.sizeWords);
 }
 
+HeapBlock* Heap_MergeFreeBlocks(u16 words)
+{
+    HeapBlock* previousBlock = nullptr;
+    HeapBlock* currentBlock = &g_heapStart;
+    const HeapBlock* const heapEnd = (HeapBlock*)(((u16*)(&g_heapStart)) + HEAP_SIZE);
+    bool trackingFreeRun = false;
+
+    for (; currentBlock < heapEnd; currentBlock = Heap_NextBlock(currentBlock))
+    {
+        if (!trackingFreeRun)
+        {
+            previousBlock = currentBlock;
+            if (!currentBlock->header.isInUse) { trackingFreeRun = true; }
+            continue;
+        }
+
+        if (!currentBlock->header.isInUse)
+        {
+            const u16 mergedWords = previousBlock->header.sizeWords + currentBlock->header.sizeWords;
+            previousBlock->header.sizeWords = mergedWords;
+            if (words <= mergedWords) { return previousBlock; }
+            continue;
+        }
+
+        if (currentBlock->header.isInUse)
+        {
+            const u16 oldFreeWords = previousBlock->header.sizeWords;
+            const u16 movedBlockWords = currentBlock->header.sizeWords;
+            const s32 movedCopyWordCount = (s32) movedBlockWords - ((sizeof(currentBlock->header.isInUse) + sizeof(currentBlock->header.sizeWords)) / WORD_SIZE);
+            const s32 movedCopyDwordCount = movedCopyWordCount / (sizeof(u32) / WORD_SIZE);
+            const s32* source = (const s32*) &currentBlock->header.owner;
+            s32* destination = (s32*) &previousBlock->header.owner;
+
+            previousBlock->header.sizeWords = movedBlockWords;
+            if (movedCopyDwordCount > 0)
+            {
+                for (s32 i = 0; i < movedCopyDwordCount; i++)
+                {
+                    destination[i] = source[i];
+                }
+            }
+
+            *previousBlock->header.owner = previousBlock->payload;
+            previousBlock->header.isInUse = true;
+
+            HeapBlock* const trailingFreeBlock = Heap_AdvanceWords(previousBlock, movedBlockWords);
+            trailingFreeBlock->header.isInUse = false;
+            trailingFreeBlock->header.sizeWords = oldFreeWords;
+
+            if (words == 0) { return nullptr; }
+
+            trackingFreeRun = false;
+            currentBlock = previousBlock;
+        }
+    }
+
+    return nullptr;
+}
+
 HeapBlock* Heap_Alloc(u16 words, void** owner)
 {
     const u16 headerWords = (sizeof(BlockHeader) / WORD_SIZE);
     const u16 totalWords = ((words + 1) & 0xFFFE) + headerWords;
-    const HeapBlock* heapEnd = (HeapBlock*)(((u16*)(&g_heapStart)) + HEAP_SIZE);
+    const HeapBlock* const heapEnd = (HeapBlock*)(((u16*)(&g_heapStart)) + HEAP_SIZE);
     HeapBlock* currBlock = &g_heapStart;
     HeapBlock* chosenBlock = nullptr;
 
--- include/extern.h
+++ include/extern.h
@@ -2,3 +2,4 @@
 
 int printf(char *fmt, ...);
 void * memset(void* dst, unsigned char ch, int count);
+void * memcpy(void* dst, const void* src, int n);
